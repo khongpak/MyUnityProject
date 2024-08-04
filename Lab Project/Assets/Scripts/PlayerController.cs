6f9bb1184bc79dec0ca88af8f8b669936e90d107
khongpak <afexim5@gmail.com>
2024-08-04 21:21:15 +0700

    Constrain the Player’s movement Code Cleanup and Export Backup

--- Lab Project/Assets/Scripts/PlayerController.cs
+++ Lab Project/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
+    private float zBound = 5.0f;
     private Rigidbody playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +17,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+        
+
+    }
+
+    //Player Move by arrow key
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRB.AddForce(Vector3.forward * speed * verticalInput);
         playerRB.AddForce(Vector3.right * speed * horizontalInput);
+    }
 
+    // Prevent the player from leaving the top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
     }
 }
