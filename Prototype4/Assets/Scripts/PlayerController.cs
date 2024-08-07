03935269f65a007ea5f61d8563bd7ceef8773588
KhongpakHomePC <afexim5@gmail.com>
2024-08-07 11:12:02 +0700

    Test for enemy and powerup Apply extra knockback with powerup Create Countdown Routine for powerup Add a powerup indicator

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private float powerupStrength = 15.0f; 
     public float speed = 5.0f;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +23,38 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true); 
+            Destroy(other.gameObject);
+
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7.0f);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigibody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.transform.position);
+            
+            Debug.Log("Collided with " + other.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigibody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
     }
 }
