36ca1f05d7194f49780b8cf2a98c92c9ceff4e61
khongpak <afexim5@gmail.com>
2024-08-08 22:02:07 +0700

    Increase enemyCount with waves Spawn Powerups with new waves BuildWebGL

--- Prototype4/Assets/Scripts/SpawnManager.cs
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
     public int enemyCount;
+    public int waveNumber = 1;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         
     }
 
@@ -21,7 +25,9 @@ public class SpawnManager : MonoBehaviour
         enemyCount = FindObjectsOfType<Enemy>().Length;
         if(enemyCount == 0)
         {
-            SpawnEnemyWave(1);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         }
     }
 
