54aa19bc163174e88dceaa55a41e7432d426ba31
khongpak <afexim5@gmail.com>
2024-08-13 21:49:24 +0700

    Create a coroutine to spawn objects Destroy target with click and sensor

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -9,7 +9,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16f;
     private float maxTorque = 10f;
     private float xRange = 4f;
-    private float ySpawnPos = -6f;
+    private float ySpawnPos = -2f;
     
     void Start()
     {
@@ -23,6 +23,16 @@ public class Target : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
     Vector3 RandomForce()
