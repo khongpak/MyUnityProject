f42e37a58307cec3b9bf6b82195fa867273b586d
KhongpakHomePC <afexim5@gmail.com>
2024-08-14 13:03:06 +0700

    Add Score text and position it on screen Edit the Score Text’s properties Initialize score text and variable Create a new UpdateScore method Add score when targets are destroyed Assign a point value to each target Add a Particle explosion

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12f;
     private float maxSpeed = 16f;
     private float maxTorque = 10f;
     private float xRange = 4f;
     private float ySpawnPos = -2f;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
     
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
@@ -28,6 +34,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
