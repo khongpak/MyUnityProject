755ecc7a2f7080b51faa9d6138022a92adea503f
khongpak <afexim5@gmail.com>
2024-09-18 22:06:52 +0700

    Implement data persistence between scenes

--- Junior Project/Assets/Scripts/Unit.cs
+++ Junior Project/Assets/Scripts/Unit.cs
@@ -26,7 +26,10 @@ public abstract class Unit : MonoBehaviour,
 
     private void Start()
     {
-
+        if(MainManager.Instance.TeamColor != null)
+        {
+            SetColor(MainManager.Instance.TeamColor);
+        }
     }
 
     void SetColor(Color c)
