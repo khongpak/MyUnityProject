e4653c98a0cb714766e5dfe7d2a80d6fd7bb9d78
KhongpakHomePC <afexim5@gmail.com>
2024-11-13 15:44:01 +0700

    AI study Acceleration

---
+++ AI_Time/Assets/Script/FixedUpdateMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedUpdateMove : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    void FixedUpdate()
+    {
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
---
+++ AI_Time/Assets/Script/LateUpdateMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LateUpdateMove : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    void LateUpdate()
+    {
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
