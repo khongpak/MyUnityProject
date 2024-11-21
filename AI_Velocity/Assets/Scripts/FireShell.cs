897275e3bf3591336d0ece2ede8321eabcf2e2ee
KhongpakHomePC <afexim5@gmail.com>
2024-11-21 15:54:38 +0700

    Update AI

--- AI_Velocity/Assets/Scripts/FireShell.cs
+++ AI_Velocity/Assets/Scripts/FireShell.cs
@@ -7,6 +7,7 @@ public class FireShell : MonoBehaviour {
     public GameObject bullet;
     public GameObject turret;
     public GameObject enemy;
+    float rotSpeed = 2;
 
     void CreateBullet() {
 
@@ -15,89 +16,14 @@ public class FireShell : MonoBehaviour {
     }
 
     void Update() {
-
+        Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-
-            Vector3 aimAt = CalculateTrajectory();
-            if (aimAt != Vector3.zero) {
-
-                this.transform.forward = CalculateTrajectory();
                 CreateBullet();
-            }
         }
     }
 
-    //Vector3 CalculateTrajectory()
-    //{
-
-    //    Vector3 p = enemy.transform.position - this.transform.position;
-    //    Vector3 v = enemy.transform.forward * enemy.GetComponent<Drive>().speed;
-    //    float s = bullet.GetComponent<MoveShell>().speed;
-
-    //    float a = Vector3.Dot(v, v) - s * s;
-    //    float b = Vector3.Dot(p, v);
-    //    float c = Vector3.Dot(p, p);
-    //    float d = b * b - a * c;
-
-    //    if (d < 0.1f) return Vector3.zero;
-
-    //    float sqrt = Mathf.Sqrt(d);
-    //    float t1 = (-b - sqrt) / c;
-    //    float t2 = (-b + sqrt) / c;
-
-    //    float t = 0.0f;
-    //    if (t1 < 0.0f && t2 < 0.0f) return Vector3.zero;
-    //    else if (t1 < 0.0f) t = t2;
-    //    else if (t2 < 0.0f) t = t1;
-    //    else
-    //    {
-
-    //        t = Mathf.Max(new float[] { t1, t2 });
-    //    }
-    //    return t * p + v;
-    //}
-
-    Vector3 CalculateTrajectory()
-    {
-        Vector3 p = enemy.transform.position - this.transform.position;
-        Vector3 v = enemy.transform.forward * enemy.GetComponent<Drive>().speed;
-        float s = bullet.GetComponent<MoveShell>().speed;
-
-        float a = Vector3.Dot(v, v) - s * s;
-        //float b = 2 * Vector3.Dot(p, v);
-        float b = Vector3.Dot(p, v);
-        float c = Vector3.Dot(p, p);
-        //float d = (a * a) - (4 * a * c);
-        float d = (b * b) - (a * c);
-
-        if (d < 0.1f)
-        {
-            return Vector3.zero;
-        }
-
-        float sqrt = Mathf.Sqrt(d);
-        float t1 = (-b - sqrt) / c;
-        float t2 = (-b + sqrt) / c;
-
-        float t = 0;
-        if (t1 < 0 && t2 < 0)
-        {
-            return Vector3.zero;
-        }
-        else if (t1 < 0)
-        {
-            t = t2;
-        }
-        else if (t2 < 0)
-        {
-            t = t1;
-        }
-        else
-        {
-            t = Mathf.Max(new float[] { t1, t2 });
-        }
-
-        return t * p + v;
-    }
+    
 }
